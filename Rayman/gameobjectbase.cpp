c4ce5da9d00e2d91611c12d4b9726ec588ffb977
fiZZowsky <piotrekr53@interia.pl>
2021-12-28 11:49:50 +0100

    Updating existing files

--- Rayman/gameobjectbase.cpp
+++
@@ -1,8 +0,0 @@
-#include "point2d.h"
-#include "gameobjectbase.h"
-#include "game_system.h"
-
-Point2D GameObjectBase::GetPosition()
-{
-	return this->position;
-}
---
+++ Settings.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define RESOLUTION_X 1920
+#define RESOLUTION_Y 1080
+
+#define V_SYNC true
+
+#define D2D_BACKGROUND_COLOR (D2D1::ColorF(0.00f, 0.60f, 0.86f, 1.0f))
